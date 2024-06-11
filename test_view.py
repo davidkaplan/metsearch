57133c177b7546152f8520227aa0c5cefb7e89a1
David Kaplan <dkappy@gmail.com>
2024-06-11 13:45:38 -0700

    Adding functionality to populate the table

--- test_view.py
+++ test_view.py
@@ -1,10 +1,27 @@
 import sys
 from PySide6.QtWidgets import QApplication
+import pprint 
 
+import rest
 import view
 
-if __name__ == "__main__":
+def test_show():
     app = QApplication(sys.argv)
     window = view.MainWindow()
     window.show()
     sys.exit(app.exec())
+
+def test_populate():
+    app = QApplication(sys.argv)
+    window = view.MainWindow()
+    window.show()
+    s = 'deluge'
+    errors, artworks = rest.searchObjects(s)
+    artworks = artworks[:80]
+    window.updateTable(artworks)
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    #test_show()
+    test_populate()
--- view.py
+++ view.py
@@ -1,6 +1,4 @@
-
-import sys
-from PySide6.QtWidgets import QApplication, QWidget, QTableWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QTableWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QAbstractItemView, QTableWidgetItem, QHeaderView
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -21,4 +19,37 @@ class MainWindow(QWidget):
         self.main_layout.addLayout(self.searchbar_layout)
         self.main_layout.addWidget(self.table)
 
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(['Preview', 'Title', 'Artist', 'Year'])
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.header = self.table.horizontalHeader()
+        self.header.setSectionResizeMode(QHeaderView.Stretch)
+        default_width = self.header.sectionSize(0)
+        self.header.setDefaultSectionSize(default_width)
+        self.header.setSectionResizeMode(QHeaderView.Interactive)
+        self.header.resizeSection(0, 50)
+
+        self.table.setSortingEnabled(True)
+        self.header.setStretchLastSection(True)
+        self.header.setSortIndicatorShown(True)
+
+        self.table.verticalHeader().setDefaultSectionSize(50)
+        self.table.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+
         self.setLayout(self.main_layout)
+        self.resize(600, 600)
+
+    def updateTable(self, items):
+        self.table.clearContents()
+        self.table.setRowCount(len(items))
+
+        for i, item in enumerate(items):
+            try:
+                self.table.setItem(i, 1, QTableWidgetItem(item['title']))
+                self.table.setItem(i, 2, QTableWidgetItem(item['artistDisplayName']))
+                self.table.setItem(i, 3, QTableWidgetItem(item['objectEndDate']))
+            except KeyError as e:
+                print('KeyError:' + str(e), item)
+
+        #self.table.resizeColumnsToContents()
+        #self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
