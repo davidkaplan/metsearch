455e6df635b2c0d5150f01b25ebab66b8c22d226
David Kaplan <dkappy@gmail.com>
2024-06-11 11:03:44 -0700

    Search and return list of objects

--- rest.py
+++ rest.py
@@ -2,11 +2,32 @@ import requests
 
 _ENDPOINT = 'https://collectionapi.metmuseum.org/public/collection/v1/'
 
-def search(search_term):
-    url = _ENDPOINT + 'search?q=' + search_term
+def _get(url):
     r = requests.get(url)
     status_code = r.status_code
     data = r.json()
+    return status_code, data
+
+def search(search_term):
+    url = _ENDPOINT + 'search?q=' + search_term
+    status_code, data = _get(url)
     total = data['total']
     ids = data['objectIDs']
     return status_code, ids
+
+def getObject(id):
+    url = _ENDPOINT + 'objects/' + str(id)
+    status_code, data = _get(url)
+    return status_code, data
+
+def getObjects(ids):
+    objects = []
+    for id in ids:
+        status_code, data = getObject(id)
+        objects.append(data)
+    return objects
+
+def searchObjects(search_term):
+    status_code, ids = search(search_term)
+    objects = getObjects(ids)
+    return objects
--- test_rest.py
+++ test_rest.py
@@ -1,4 +1,5 @@
 import rest
+import pprint
 
 def test_search():
     s = 'deluge'
@@ -6,5 +7,12 @@ def test_search():
     print(status_code)
     print(ids)
 
+def test_objects():
+    s = 'deluge'
+    objects = rest.searchObjects(s)
+    print('Number of Objects:', len(objects))
+    pprint.pp(objects[0])
+
 if __name__ == '__main__':
-    test_search()
+    #test_search()
+    test_objects()
