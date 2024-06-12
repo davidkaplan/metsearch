9ddde4b75e4e9222895eb0d0a2f47fed09b46593
David Kaplan <dkappy@gmail.com>
2024-06-11 20:17:22 -0700

    Create disk image using create-dmg

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+
 rm -rf build/ dist/ ../__pycache__/
 rm MET\ Search.spec
 pyinstaller --name "MET Search" --windowed metsearch.py
+
+test -f "dist/MET Search.dmg" && rm "dist/MET Search.dmg"
+create-dmg \
+  --volname "MET Search" \
+  --hide-extension "MET Search.app" \
+  "dist/MET Search.dmg" \
+  "dist/MET Search.app/"
