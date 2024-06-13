296139e524862f2303daaa251cb2576b897ae244
David Kaplan <dkappy@gmail.com>
2024-06-13 11:34:07 -0700

    Add classification and image filtering to UI and table

--- metsearch/view.py
+++ metsearch/view.py
@@ -1,10 +1,11 @@
-from PySide6.QtWidgets import QWidget, QTableWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QAbstractItemView, QTableWidgetItem, QHeaderView, QCheckBox
+from PySide6.QtWidgets import QWidget, QTableWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QAbstractItemView, QTableWidgetItem, QHeaderView, QCheckBox, QGroupBox, QComboBox, QSizePolicy
 from PySide6.QtCore import QObject, Signal, QThreadPool, QRunnable, QSize
 from PySide6.QtGui import QPixmap
 
 from metsearch import rest
 
 _ROW_HEIGHT = 100 # pixels
+_CLASSIFICATION_DEFAULT_STR = 'ALL CLASSIFICATIONS'
 
 class ImageDownloaderSignals(QObject):
     finished = Signal()
@@ -28,23 +29,40 @@ class MainWindow(QWidget):
 
         self.label = QLabel('Search')
         self.search = QLineEdit('')
-        self.hasImageCheckBox = QCheckBox('Has Image')
         self.searchButton = QPushButton("Search")
         self.searchButton.clicked.connect(self.doSearch)
 
         self.searchbar_layout = QHBoxLayout()
         self.searchbar_layout.addWidget(self.label)
         self.searchbar_layout.addWidget(self.search)
-        self.searchbar_layout.addWidget(self.hasImageCheckBox)
         self.searchbar_layout.addWidget(self.searchButton)
+
+        self.filter_layout = QHBoxLayout()
+        self.hasImageCheckBox = QCheckBox('Has Image')
+        self.hasImageCheckBox.setFixedWidth(100)
+        self.hasImageCheckBox.clicked.connect(self.filter)
+        self.classificationCheckBox = QCheckBox('Classification')
+        self.classificationCheckBox.setFixedWidth(105)
+        self.classificationCheckBox.clicked.connect(self.filter)
+        self.classificationComboBox = QComboBox()
+        self.classificationComboBox.setEnabled(False)
+        self.classificationComboBox.addItem(_CLASSIFICATION_DEFAULT_STR)
+        self.classificationComboBox.currentTextChanged.connect(self.filter)
+        
+        self.filter_layout.addWidget(self.hasImageCheckBox)
+        self.filter_layout.addWidget(self.classificationCheckBox)
+        self.filter_layout.addWidget(self.classificationComboBox)
+        self.filter_group = QGroupBox('Filter By')
+        self.filter_group.setLayout(self.filter_layout)
 
         self.table = QTableWidget()
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(self.searchbar_layout)
+        self.main_layout.addWidget(self.filter_group)
         self.main_layout.addWidget(self.table)
 
-        self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(['Preview', 'Title', 'Artist', 'Date'])
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(['Preview', 'Title', 'Artist', 'Date', 'Classification'])
         self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.header = self.table.horizontalHeader()
         self.header.setSectionResizeMode(QHeaderView.Stretch)
@@ -52,6 +70,7 @@ class MainWindow(QWidget):
         self.header.setDefaultSectionSize(default_width)
         self.header.setSectionResizeMode(QHeaderView.Interactive)
         self.header.resizeSection(0, _ROW_HEIGHT)
+        self.header.resizeSection(3, 80)
         self.table.setIconSize(QSize(_ROW_HEIGHT, _ROW_HEIGHT))
 
         self.table.setSortingEnabled(True)
@@ -65,10 +84,14 @@ class MainWindow(QWidget):
         self.resize(600, 600)
                                                 
         self.threadpool = QThreadPool()
+        
+
 
     def updateTable(self, items):
         self.table.clearContents()
         self.table.setRowCount(len(items))
+
+        classifications = [_CLASSIFICATION_DEFAULT_STR]
 
         for i, item in enumerate(items):
             try:
@@ -76,6 +99,10 @@ class MainWindow(QWidget):
                 self.table.setItem(i, 1, QTableWidgetItem(item['title']))
                 self.table.setItem(i, 2, QTableWidgetItem(item['artistDisplayName']))
                 self.table.setItem(i, 3, QTableWidgetItem(str(item['objectEndDate'])))
+                classification = item['classification']
+                self.table.setItem(i, 4, QTableWidgetItem(classification))
+                if classification not in classifications and not classification == '':
+                    classifications.append(classification)
             except KeyError as e:
                 print('KeyError:' + str(e), item)
 
@@ -92,6 +119,13 @@ class MainWindow(QWidget):
         # reset scroll to top
         self.table.scrollToItem(self.table.item(0, 0))
 
+        # Create classifications dropdown options
+        self.classificationComboBox.blockSignals(True)
+        self.classificationComboBox.clear()
+        for classification in sorted(classifications):
+            self.classificationComboBox.addItem(classification)
+        self.classificationComboBox.blockSignals(False)
+
     def updateThumbnail(self, filename, tableitem):
         if filename and not filename == '':
             thumb_pixmap = QPixmap(filename)
@@ -105,4 +139,22 @@ class MainWindow(QWidget):
         hasImage = self.hasImageCheckBox.isChecked()
         errors, artworks = rest.searchObjects(search_string, imagesOnly=hasImage)
         self.updateTable(artworks)
+
+    def filter(self):
+        images_only = self.hasImageCheckBox.isChecked()
+        by_classification = self.classificationCheckBox.isChecked()
+        classification_str = self.classificationComboBox.currentText()
+        self.classificationComboBox.setEnabled(by_classification)
+
+        for i in range(self.table.rowCount()):
+            self.table.showRow(i)
+            if images_only:
+                if self.table.item(i, 0).icon().isNull():
+                    self.table.hideRow(i)
+            if by_classification and not classification_str == _CLASSIFICATION_DEFAULT_STR:
+                if not self.table.item(i, 4).text() == classification_str:
+                    self.table.hideRow(i)
+                
+
+
 
